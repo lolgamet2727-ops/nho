ad8c32bb17987cf7d76442126fd9114be149c385
lolgamet2727-ops <lolgamet.2727@gmail.com>
2026-02-04 03:42:44 +0100

    Remove audio playback functionality from script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Link audio ultra-stabile
-const audio = new Audio('https://www.soundhelix.com'); 
-// Sostituisci il link sopra con quello di Patrick Watson se ne hai uno .mp3 diretto
-audio.loop = true;
-
-document.getElementById('playButton').addEventListener('click', function() {
-    if (audio.paused) {
-        audio.play();
-        this.innerText = "In riproduzione... 💖";
-    } else {
-        audio.pause();
-        this.innerText = "Premi per la musica 🎵";
-    }
-});
 
